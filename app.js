157f9ab989edc333b3a97c5f68e099fa2e3c8d6e
Nihad <mncnihad@gmail.com>
2024-11-10 15:07:22 +0530

    created Layout main hbs and app.js set hbs file

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const path = require("path");
+const exphbs = require("express-handlebars");
 
 const app = express();
+
 dotenv.config();
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -12,9 +16,24 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to MongoDB:", err));
 
-const port = process.env.PORT_NO;
+app.engine(
+  "hbs",
+  exphbs.engine({
+    extname: "hbs",
+    defaultLayout: "main",
+    layoutsDir: path.join(__dirname, "views", "layouts"),
+  })
+);
+
+app.set("view engine", "hbs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(express.json());
 
+const port = process.env.PORT_NO;
+
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
