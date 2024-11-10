9ae1f579344989e1071ef1778b84735716013918
Nihad <mncnihad@gmail.com>
2024-11-10 12:41:43 +0530

    server is created

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT_NO;
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
