acfb4d366f308906aade406e6d02e0b30afc6495
Nihad <mncnihad@gmail.com>
2024-11-10 20:48:29 +0530

    created User model mongoose schema

---
+++ model/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
