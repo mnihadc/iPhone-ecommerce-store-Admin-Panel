4783e3fea95d5c11e91cabe86e9faf27a888f34c
Nihad <mncnihad@gmail.com>
2024-11-12 08:51:04 +0530

    jsonwebtoken implement in login function

--- controller/home.controller.js
+++ controller/home.controller.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const getHomePage = (req, res, next) => {
   res.render("Home", {
     title: "Home page",
@@ -16,10 +17,20 @@ const Login = (req, res, next) => {
     const { email, password } = req.body;
     const emailData = process.env.email;
     const passwordData = process.env.password;
+
     if (emailData === email && passwordData === password) {
+      const token = jwt.sign({ email: email }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+      res.cookie("authToken", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 3600000,
+      });
       res.redirect("/");
     } else {
-      return res.status(404).send("Invalid email or password");
+      return res.send("Invalid email or password");
     }
   } catch (error) {
     next(error);
