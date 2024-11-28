e16bfd6c9dffa96565e832950e70af6fd141c958
Nihad <mncnihad@gmail.com>
2024-11-28 19:45:28 +0530

    created Admin auth mongoose Schema modal

---
+++ model/Admin.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const adminSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    profileImage: {
+      type: String,
+      default:
+        "https://www.pngmart.com/files/21/Admin-Profile-Vector-PNG-Image.png",
+    },
+    role: {
+      type: String,
+      enum: ["Super Admin", "Admin"],
+      default: "Admin",
+    },
+    dateJoined: {
+      type: Date,
+      default: Date.now,
+    },
+    lastLogin: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Password Hashing Middleware
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Compare Password Method
+adminSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model("Admin", adminSchema);
